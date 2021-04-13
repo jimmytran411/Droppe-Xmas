10fc81e6bba322bf5a2a7dfabafc1db53255677c
An Tran <jimmytran411@gmail.com>
2021-04-13 15:22:13 +0300

    fix header now showing totalPrice and saving when not in Checkout

--- src/Header.tsx
+++ src/Header.tsx
@@ -37,6 +37,7 @@ export const Header = ({ handleCheckout }: IHeader) => {
                 onClick={() => {
                   handleOpenWishList(wishlist);
                   handleCheckout(false);
+                  setTotalTextGroup(true);
                 }}
               >{`Child ${wishlist.id}`}</button>
             );
